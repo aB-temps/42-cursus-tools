38c922b929ce4fe9578641ce78d23f4fa2a8cedf
aB-temps <abetemps@student.42lyon.fr>
2025-05-06 00:47:50 +0200

    commit_maker functionnal

--- scripts/commit_maker.sh
+++ scripts/commit_maker.sh
@@ -1,4 +1,119 @@
-#!/bin/zsh
+#!/bin/bash
 
-local wd=$PWD
-echo $wd
+reset="\e[0m"
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+white="\e[39m"
+cyan="\e[96m"
+bold="\e[1m"
+dim="\e[2m"
+underline="\e[4m"
+
+welcome="⚡️ $bold Weclome to $cyan commit_maker$reset ⚡️\n"
+types=("feat" "fix" "style" "structure" "refacto" "merge" "doc")
+emojis=("✨" "🔧" "🎨" "🏗️" "♻️" "🔀" "📝")
+selected=0
+
+print_box() {
+  local size=${#1}
+  local text=$1
+  local bc=$2
+  local tc=$3
+  echo -en "$bc╔"
+  for ((i = 0; i < size + 2; i++)); do
+    echo -n ═
+  done
+  echo -e "╗$reset"
+  echo -en "$bc║ $reset"
+  echo -en "$tc$text$reset"
+  echo -e " $bc║$reset"
+  echo -en "$bc╚"
+  for ((i = 0; i < size + 2; i++)); do
+    echo -n ═
+  done
+  echo -e "╝$reset"
+}
+
+clear_and_print() {
+  clear -x
+  echo -e $1
+}
+
+commit_prev() {
+  clear_and_print "$welcome"
+  print_box $1 $2 $3
+  echo
+}
+
+print_menu() {
+  clear_and_print "$welcome"
+  print_box "Use ↑/↓ to choose, Enter to select:" $dim $dim
+  echo
+  tput civis
+  for i in "${!types[@]}"; do
+    if [[ $i -eq $selected ]]; then
+      echo -e "$bold$cyan> ${types[$i]}$reset"
+    else
+      echo "  ${types[$i]}"
+    fi
+  done
+}
+
+# START
+
+# DEFINE TYPE
+while true; do
+  print_menu
+  read -rsn1 key
+  if [[ $key == $'\x1b' ]]; then
+    read -rsn2 key
+    case "$key" in
+    '[A') ((selected--)) ;;
+    '[B') ((selected++)) ;;
+    esac
+  elif [[ $key == "" ]]; then
+    break
+  fi
+
+  ((selected < 0)) && selected=$((${#types[@]} - 1))
+  ((selected >= ${#types[@]})) && selected=0
+done
+
+type=${types[$selected]}
+emoji=${emojis[$selected]}
+message="$type"
+
+tput reset
+commit_prev $message $white $bold$cyan
+echo -e "$green>$reset Type : $green$type$reset"
+echo -ne "$cyan>$reset Scope : $cyan"
+
+# DEFINE SCOPE
+read scope
+echo -e $reset
+if [[ -z $scope ]]; then
+  message+=":"
+else
+  message+="($scope):"
+fi
+
+commit_prev $message $white $bold$cyan
+echo -e "$green>$reset Type : $green$type$reset"
+if [[ -z $scope ]]; then
+  echo -e "$red>$reset Scope : $dim no scope$reset"
+else
+  echo -e "$green>$reset Scope : $green$scope$reset"
+fi
+
+# DEFINE DESC
+echo -ne "$cyan>$reset Description : $cyan"
+read desc
+while [[ -z $desc ]]; do
+  echo -ne "$cyan>$reset Description : $cyan"
+  read desc
+done
+echo -e $reset
+message+=$desc
+
+commit_prev $message
