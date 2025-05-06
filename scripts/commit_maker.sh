67fa2abf8a98b46ae7266574a1fa76657d7c23af
aB-temps <abetemps@student.42lyon.fr>
2025-05-06 02:29:41 +0200

    rework finished

--- scripts/commit_maker.sh
+++ scripts/commit_maker.sh
@@ -12,39 +12,12 @@ dim="\e[2m"
 underline="\e[4m"
 
 # VAR =================================================================
-welcome="⚡️ ${bold}Weclome to ${cyan}commit_maker$reset ⚡️\n"
+welcome="⚡️ ${bold}Weclome to ${cyan}commit_maker${reset} ⚡️\n"
 types=("new" "fix" "refactor" "structure" "style" "merge" "doc")
 emojis=("✨" "🔧" "♻️ " "🏗️ " "🎨" "🔀" "📝")
 selected=0
 
 # FUNC ================================================================
-print_box() {
-  local args=("$@")
-  local color=${args[-1]}
-  unset 'args[-1]'
-  local size=0
-
-  for arg in "${args[@]}"; do
-    size=$((size + ${#arg} + 1))
-  done
-  size=$((size - 1))
-  echo -en "$color╔"
-  for ((i = 0; i < size + 2; i++)); do
-    echo -n ═
-  done
-  echo -e "╗$reset"
-  echo -en "$color║ $reset"
-  for arg in "${args[@]}"; do
-    echo -en "$color$arg"
-  done
-  echo -e "$color ║$reset"
-  echo -en "$color╚"
-  for ((i = 0; i < size + 2; i++)); do
-    echo -n ═
-  done
-  echo -e "╝$reset"
-}
-
 clear_and_print() {
   clear
   echo -e $1
@@ -52,18 +25,16 @@ clear_and_print() {
 
 commit_prev() {
   clear_and_print "$welcome"
-  print_box $1 $2
-  echo
+  echo -e "➡️   [${bold}$1$2${reset}]\n"
 }
 
 print_menu() {
   clear_and_print "$welcome"
-  print_box "Use ↑/↓ to choose, Enter to select:" $dim
+  echo -e "${dim}Use ↑/↓ to choose, Enter to select:${reset}"
   echo
-  tput civis
   for i in "${!types[@]}"; do
     if [[ $i -eq $selected ]]; then
-      echo -e "$bold$cyan> ${types[$i]}$reset"
+      echo -e "$bold$cyan> ${types[$i]}${reset}"
     else
       echo "  ${types[$i]}"
     fi
@@ -75,6 +46,7 @@ print_menu() {
 # =====================================================================
 
 # DEFINE TYPE =========================================================
+tput civis
 while true; do
   print_menu
   read -rsn1 key
@@ -94,48 +66,46 @@ done
 
 type=${types[$selected]}
 emoji=${emojis[$selected]}
-message="$type"
+message="$emoji $type"
 
 tput reset
-commit_prev $message $dim$cyan
-echo -e "$green>$reset Type : $green$type$reset"
-echo -ne "$cyan>$reset Scope : $cyan"
+commit_prev $green "$message ${dim}${cyan}..."
+echo -e "$green>${reset} Type : $green$type${reset}"
+echo -ne "$cyan>${reset} Scope : $cyan"
 
 # DEFINE SCOPE ========================================================
 read scope
-echo -e $reset
+echo -e ${reset}
 if [[ -z $scope ]]; then
-  message+=":"
+  message+=": "
 else
-  message+="($scope):"
+  message+="($scope): "
 fi
 
-commit_prev $message $dim$cyan
-echo -e "$green>$reset Type : $green$type$reset"
+commit_prev $green "$message ${dim}${cyan}..."
+echo -e "$green>${reset} Type : $green$type${reset}"
 if [[ -z $scope ]]; then
-  echo -e "$red>$reset Scope : {$dim}no scope$reset"
+  echo -e "$red>${reset} Scope : ${dim}no scope${reset}"
 else
-  echo -e "$green>$reset Scope : $green$scope$reset"
+  echo -e "$green>${reset} Scope : $green$scope${reset}"
 fi
 
 # DEFINE DESC =========================================================
-echo -ne "$cyan>$reset Description : $cyan"
-read desc
 while [[ -z $desc ]]; do
-  echo -ne "$cyan>$reset Description : $cyan"
+  echo -ne "$cyan>${reset} Description : $cyan"
   read desc
 done
-echo -e "$reset\n"
-message+=$desc
-commit_prev $message $dim$cyan
-echo -e "$green>$reset Type : $green$type$reset"
+echo -e "${reset}\n"
+message+="$desc"
+commit_prev $green "$message"
+echo -e "$green>${reset} Type : $green$type${reset}"
 if [[ -z $scope ]]; then
-  echo -e "$red>$reset Scope : {$dim}no scope$reset"
+  echo -e "$red>${reset} Scope : ${dim}no scope${reset}"
 else
-  echo -e "$green>$reset Scope : $green$scope$reset"
+  echo -e "$green>${reset} Scope : $green$scope${reset}"
 fi
-echo -e "$green>$reset Description : $green$desc$reset\n$dim"
-echo -e "Press Enter to contiue ...$reset"
+echo -e "$green>${reset} Description : $green$desc${reset}\n$dim"
+echo -e "Press Enter to contiue ...${reset}"
 tput civis
 
 read -rsn1 key
@@ -147,17 +117,14 @@ tput reset
 #CONFIRM ==============================================================
 echo -e $welcome
 
-full_message="$emoji $message"
-echo -e "$full_message\n"
-echo -ne "Confirm commit message [${green}y$reset/${red}n$reset] : "
-read -n1 conf
+commit_prev $green "$message"
 while [[ $conf != "y" && $conf != "n" ]]; do
-  echo -ne "Confirm commit message [${green}y$reset/${red}n$reset] : "
+  echo -ne "Confirm commit message [${green}y${reset}/${red}n${reset}] : "
   read -n1 conf
+  echo -e "${reset}\n"
 done
-echo
 if [[ $conf == 'n' ]]; then
   echo -e "❌ ${red}Commit aborted${reset} ❌\n"
 else
-  echo -e "git commit -m \"$full_message\"\n"
+  echo -e "git commit -m \"$message\"\n"
 fi
